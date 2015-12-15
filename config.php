c0aa5bc0f022c1051d81c2de31b54d77c78a01c0
Andrey Yatsuk <hi@yatsukav.com>
2015-12-15 15:45:32 +0300

    Ver3
    • Перешел с функциональной парадигмы на ООП парадигму
    • Общение с сервером идет через обращение к переменной $DataBase,
    находящейся внутри файла с описанием работы БД. Так интуитивнее будет
    сменить одну БД на другую.

--- config.php
+++ config.php
@@ -1,10 +1,57 @@
 <?php
 	// Берем данные из конфига
-	$ini_array = parse_ini_file("config.cfg");
-	
-	$userDB 	= $ini_array[user];
-	$passwordDB = $ini_array[password];
-	$nameDB 	= $ini_array[db];
-	$hostDB		= $ini_array[host];
-	$portDB		= $ini_array[port];
+	class SPConfig
+	{
+		private $ini_array;
+
+		private $userDB;
+		private $passwordDB;
+		private $nameDB;
+		private $hostDB;
+		private $portDB;
+		private $charset;
+
+		public function __construct(){
+			$this->ini_array = parse_ini_file("config.cfg");
+
+			$this->userDB 		= $this->ini_array['user'];
+			$this->passwordDB 	= $this->ini_array['password'];
+			$this->nameDB		= $this->ini_array['db'];
+			$this->hostDB		= $this->ini_array['host'];
+			$this->portDB		= $this->ini_array['port'];
+			$this->charset		= $this->ini_array['charset'];
+		}
+
+		public function getUserDB()
+		{
+			return $this->userDB;
+		}
+
+		public function getPasswordDB()
+		{
+			return $this->passwordDB;
+		}
+
+		public function getNameDB()
+		{
+			return $this->nameDB;
+		}
+
+		public function getHostDB()
+		{
+			return $this->hostDB;
+		}
+
+		public function getPortDB()
+		{
+			return $this->portDB;
+		}
+
+		public function getCharset()
+		{
+			return $this->charset;
+		}
+	}
+
+	$Config = new SPConfig();
 ?>
--- mysql.php
+++ mysql.php
@@ -1,106 +1,120 @@
 <?php
 	require_once("config.php");
-	
-	////
-	// Подключение к MySQL
-	function yav_mysql_init() {
-		// from config.php
-		global $hostDB, $portDB, $userDB, $passwordDB, $nameDB;
-		// Connect to MySQL
-		$link = mysql_connect(
-			"$hostDB:$portDB", 
-			$userDB, 
-			$passwordDB
-		) or die (mysql_error ());
-		// Connect to data base
-		mysql_select_db(
-			$nameDB, 
-			$link
-		) or die(mysql_error());
-		// Setting charset
-		mysql_set_charset("utf8", $link);
-		// Return $link
-		return $link;
-	}
-	
-	////
-	// Вспомогательная функция. Возвращает массив со всеми значениями ответа БД
-	function yav_getArrayByQuery($query){
-		$i = 0;
-		while($result = mysql_fetch_assoc($query)){	    
-		    $array[$i] = $result;
-		    $i++;
+
+	class SBMySQL
+	{
+		private $link;
+
+		////
+		// Конструктор
+		// Подключение к MySQL
+		public function __construct(){
+			// from config.php
+			global $Config;
+			// Connect to MySQL
+			$this->link = mysql_connect(
+					$Config->getHostDB() . ":" . $Config->getPortDB(),
+					$Config->getUserDB(),
+					$Config->getPasswordDB()
+			) or die (mysql_error ());
+			// Connect to data base
+			mysql_select_db(
+					$Config->getNameDB(),
+					$this->link
+			) or die(mysql_error());
+			// Setting charset
+			mysql_set_charset($Config->getCharset(), $this->link);
 		}
-		return $array;		
+
+		////
+		// Деструктор
+		public function __destruct(){
+			mysql_close($this->link);
+		}
+
+		////
+		// Вспомогательная функция. Возвращает массив со всеми значениями ответа БД
+		private function getArrayByQuery($query){
+			$array = [];
+			$i = 0;
+			while($result = mysql_fetch_assoc($query)){
+				$array[$i] = $result;
+				$i++;
+			}
+			return $array;
+		}
+
+		////
+		// Показывает новости
+		public function getPosts($count=10, $offset=0){
+			$query = mysql_query("SELECT * FROM posts ORDER BY id DESC LIMIT $count OFFSET $offset");
+			return $this->getArrayByQuery($query);
+		}
+
+		////
+		// Новость по ID
+		public function getPostByID($id){
+			$query = mysql_query("SELECT * FROM posts WHERE id='$id'");
+			return mysql_fetch_assoc($query);
+		}
+
+		////
+		// Поиск
+		public function getPostsBySubstring($text) {
+			$query = mysql_query("SELECT * FROM posts
+								WHERE title LIKE '%$text%'
+								OR content LIKE '%$text%'
+								OR category LIKE '%$text%'
+								ORDER BY id DESC");
+			return $this->getArrayByQuery($query);
+		}
+
+		////
+		// Показывает новости по выбранной категории
+		public function getPostsByCat($category){
+			$query = mysql_query("SELECT * FROM posts WHERE category='$category' ORDER BY id DESC");
+			return $this->getArrayByQuery($query);
+		}
+
+		////
+		// Выдает список всех категорий
+		public function getCategories(){
+			$query = mysql_query("SELECT DISTINCT category FROM posts ORDER BY category");
+			return $this->getArrayByQuery($query);
+		}
+
+		////
+		// Добавление новости
+		public function PostAdd($title, $content, $category) {
+			$title = mysql_escape_string($title);
+			$content = mysql_escape_string($content);
+			$category = mysql_escape_string($category);
+			mysql_query("INSERT INTO posts (title, content, category)
+						VALUES ('$title', '$content', '$category')");
+		}
+
+		////
+		// Редактирование новости
+		public function PostUpdate($id, $title, $content, $category) {
+			mysql_query("UPDATE posts
+						SET title='$title', content='$content', category='$category'
+						WHERE id='$id'");
+		}
+
+		////
+		// Удаление новости
+		public function PostDelete($id) {
+			mysql_query("DELETE FROM posts WHERE id='$id'");
+		}
+
+		////
+		// Количество новостей
+		public function getPostsCount() {
+			$query = mysql_query("SELECT COUNT(*) FROM posts");
+			return mysql_fetch_row($query)[0];
+		}
 	}
-	
-	////
-	// Показывает новости
-	function yav_mysql_show($count=10, $offset=0){
-		$query = mysql_query("SELECT * FROM posts ORDER BY id DESC LIMIT $count OFFSET $offset");
-		return yav_getArrayByQuery($query);
-	}
-	
-	////
-	// Новость по ID
-	function yav_mysql_byID($id){
-		$query = mysql_query("SELECT * FROM posts WHERE id='$id'");
-		return mysql_fetch_assoc($query);
-	}
-	
-	////
-	// Показывает новости по выбранной категории
-	function yav_mysql_byCat($category){
-		$query = mysql_query("SELECT * FROM posts WHERE category='$category' ORDER BY id DESC");
-		return yav_getArrayByQuery($query);		
-	}
-	
-	////
-	// Выдает список всех категорий
-	function yav_mysql_getCategories(){
-		$query = mysql_query("SELECT DISTINCT category FROM posts ORDER BY category");
-		return yav_getArrayByQuery($query);			
-	}
-	
-	////
-	// Добавление новости
-	function yav_mysql_add($title, $content, $category) {
-		$title = mysql_escape_string($title);
-		$content = mysql_escape_string($content);
-		$category = mysql_escape_string($category);
-		$query = mysql_query("INSERT INTO posts (title, content, category)
-							VALUES ('$title', '$content', '$category')");
-	}
-	
-	////
-	// Редактирование новости
-	function yav_mysql_edit($id, $title, $content, $category) {
-		$query = mysql_query("UPDATE posts
-							SET title='$title', content='$content', category='$category'
-							WHERE id='$id'");	
-	}
-	
-	////
-	// Удаление новости
-	function yav_mysql_del($id) {
-		$query = mysql_query("DELETE FROM posts WHERE id='$id'");
-	}
-	
-	////
-	// Поиск
-	function yav_mysql_search($text) {
-		$query = mysql_query("SELECT * FROM posts 
-							WHERE title LIKE '%$text%'
-							OR content LIKE '%$text%' 
-							OR category LIKE '%$text%'
-							ORDER BY id DESC");
-		return yav_getArrayByQuery($query);
-	}
-	
-	////
-	// Количество новостей
-	function yav_mysql_count() {
-		$query = mysql_query("SELECT COUNT(*) FROM posts");
-		return mysql_fetch_row($query)[0];		
-	}
+
+	// Создаем единственный экземпляр БД
+	$DataBase = new SBMySQL();
 ?>
